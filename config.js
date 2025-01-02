233740ef70d89136924dcbade6149440c4b5cfa5
lajitj <134612459+phantom517@users.noreply.github.com>
2025-01-02 06:57:05 +0545

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ module.exports = {
     minecraft: {
       host: 'tbs.ferox.host', // Replace with your Minecraft server's host
       port: 26044,         // Replace with your Minecraft server's port
-      username: 'IdleThrone77',
+      username: 'PHNTOMXD',
       auth: 'microsoft',   // Microsoft authentication (e.g., 'microsoft' or 'offline')
       profilesFolder: './auth', // Where to store authentication sessions
 
     },
     discord: {
-        token:'MTI5ODUwMTY5MTMzOTA0NjkyNA.GXZBJL.reqMS9sJyOjXA_vM9PiFf6DEayGsKb3oXJv09E',
+        token:'MTI5ODUwMTY5MTMzOTA0NjkyNA.G8yH96.doELD5T6V2xcJ_PQQ_uFvn8EelY9-ab0AqZzI8',
         channelId:'1298500246262906904',   // Replace with your target Discord channel ID
     },
   };
